4130c59f69e02368b8544a16f16dceccf4ab8997
Nurul Arifah <ifarifahh@gmail.com>
2021-06-15 12:08:26 +0700

    pekan 3

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialDesign } from './material/material';
+import { ForgotComponent } from './auth/forgot/forgot.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialDesign
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
