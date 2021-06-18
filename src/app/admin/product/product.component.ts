488b0ca651e54ff2804980f05b5040666e6624a4
Nurul Arifah <ifarifahh@gmail.com>
2021-06-18 20:58:37 +0700

    pekan 4

--- src/app/admin/product/product.component.ts
+++ src/app/admin/product/product.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
+  title:any;
+  barang:any={};
 
   constructor() { }
 
   ngOnInit(): void {
+    this.title='Product';
   }
 
 }
