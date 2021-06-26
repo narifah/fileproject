c9e24b53454d94f75ac3e2a9888a1025f16ca836
Nurul Arifah <ifarifahh@gmail.com>
2021-06-26 16:08:44 +0700

    pekan 5

--- src/app/admin/product/product.component.ts
+++ src/app/admin/product/product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ProductDetailComponent } from '../product-detail/product-detail.component';
 
 @Component({
   selector: 'app-product',
@@ -8,11 +10,32 @@ import { Component, OnInit } from '@angular/core';
 export class ProductComponent implements OnInit {
   title:any;
   barang:any={};
+  barangs:any=[]
 
-  constructor() { }
+  constructor(
+    public dialog:MatDialog
+  ) { }
 
   ngOnInit(): void {
     this.title='Product';
+  }
+
+
+  productDetail(data: any,idx: number)
+  {
+   let dialog=this.dialog.open(ProductDetailComponent, {
+     width:'400px',
+     data:data
+   });
+   dialog.afterClosed().subscribe(res=>{
+     if(res)
+     {
+        //jika idx=-1 (penambahan data baru) maka tambahkan data
+       if(idx==-1)this.barangs.push(res);      
+        //jika tidak maka perbarui data  
+       else this.barangs[idx]=res; 
+     }
+   })
   }
 
 }
